314c0089cc07976ae83810687ebdf8598165792b
Shivang Agarwal <shivangagarwal11.sa@gmail.com>
2020-10-24 19:54:41 +0530

    Foodish Site

---
+++ resources/js/script.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+   $(".js--class-features").waypoint(function(direction){
+    if(direction=="down"){
+        $('nav').addClass("sticky");
+        
+    }else{
+        $('nav').removeClass('sticky');
+    }
+},{
+    offset:'50px;'
+   });
+   $(".js--scroll-to-plans").click(function(){
+   $('html,body').animate({scrollTop:$('.js--section-plan').offset().top},1000); 
+});
+   $(".js--scroll-to-start").click(function(){
+   $('html,body').animate({scrollTop:$('.js--class-features').offset().top},1000); 
+});
+$('a[href*="#"]')
+// Remove links that don't actually link to anything
+.not('[href="#"]')
+.not('[href="#0"]')
+.click(function(event) {
+  // On-page links
+  if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+    && 
+    location.hostname == this.hostname
+  ) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000, function() {
+        // Callback after animation
+        // Must change focus!
+        var $target = $(target);
+        $target.focus();
+        if ($target.is(":focus")) { // Checking if the target was focused
+          return false;
+        } else {
+          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+          $target.focus(); // Set focus again
+        };
+      });
+    }
+  }
+});
+$('.js--wp-1').waypoint(function(direction){
+  $('.js--wp-1').addClass('animated fadeIn');
+},{
+  offset:"50%"
+});
+$('.js--wp-2').waypoint(function(direction){
+  $('.js--wp-2').addClass('animated fadeInUp');
+},{
+  offset:"50%"
+});
+$('.js--wp-3').waypoint(function(direction){
+  $('.js--wp-3').addClass('animated pulse');
+},{
+  offset:"50%"
+});
+$('.js--nav-icon').click(function(){
+  var x=$('.js--main-nav');
+  x.slideToggle(200);
+});
+});
